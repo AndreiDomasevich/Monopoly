2ae531588fcfaded07aadf40ca40d5683aae2a78
Сергей Гордиевич < >
2016-11-14 13:51:20 +0300

    Menu design

---
+++ app/src/main/java/com/example/gardi/monopoly/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.gardi.monopoly;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.spash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent menuIntent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(menuIntent);
+                finish();
+            }
+        },2*1000);
+    }
+}
